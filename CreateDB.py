bcff2ddcd040802c0bfb63d438152d70607f3a09
Yash Raghav <1yashraghav@gmail.com>
2020-10-18 15:30:14 +0530

    CreatedDB

--- CreateDB.py
+++ CreateDB.py
@@ -4,25 +4,27 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-Base = declarative_base
-#***************************************  Starting  ***************************************
+
+Base = declarative_base()
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
 
-class Menu_item(Base):
-    __tablename__ = 'menu_item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item=-
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
-#***************************************  Same ending ***************************************
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
-
